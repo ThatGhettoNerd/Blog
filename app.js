f403f7d4b651fe682c37590e1c77ef38dd8a32bf
John Goulart <thatghettonerd@gmail.com>
2020-05-17 07:46:06 -0700

    finished home page and contact sections

---
+++ app.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+const navMenu = document.querySelector('.main-menu');
+const menuIcon = document.querySelector('.menu-icon');
+
+menuIcon.addEventListener('click', function() {
+    navMenu.classList.toggle('slideIn');
+});
+
